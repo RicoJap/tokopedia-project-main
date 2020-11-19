96ac0d3a6b7d396e8bcb59f2a8be16d0f1b16bd4
RicoJap <ricojap@gmail.com>
2020-11-19 19:57:37 +0700

    Add nothing to show page

---
+++ src/components/NothingToShow/NothingToShow.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Pokeball from "../../assets/pokeball-big.png";
+import Image from "../Image/Image";
+import Typography from "../Typography/Typography";
+
+const styles = {
+  image: {
+    height: 100,
+    width: 100,
+    display: "block",
+    margin: "auto",
+    paddingTop: "50%",
+  },
+  label: {
+    display: "block",
+    textAlign: "center",
+    margin: "1em",
+  },
+};
+
+const NothingToShow = ({ label }) => {
+  return (
+    <div>
+      <Image src={Pokeball} variant="square" style={styles.image} />
+      <Typography
+        variant="p"
+        color="textSecondary"
+        label={label}
+        style={styles.label}
+      />
+    </div>
+  );
+};
+
+export default NothingToShow;
--- src/containers/PokemonsList/PokemonsList.jsx
+++ src/containers/PokemonsList/PokemonsList.jsx
@@ -6,95 +6,93 @@ import { toTitleCase } from "../../utilities/string.utils";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import {
-    fetchPokemonDetail,
-    fetchPokemonsList,
-    setCurrentPokemonsListPage,
+  fetchPokemonDetail,
+  fetchPokemonsList,
+  setCurrentPokemonsListPage,
 } from "../../actions/actions";
 import { ceiling } from "../../utilities/number.utils";
 import { isEmpty } from "../../utilities/check-if-empty.utils";
 
 const PokemonsList = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const [
-        currentlyDisplayedPokemonsList,
-        setCurrentlyDisplayedPokemonsList,
-    ] = useState([]);
+  const [
+    currentlyDisplayedPokemonsList,
+    setCurrentlyDisplayedPokemonsList,
+  ] = useState([]);
 
-    const allPokemonsList = useSelector((state) => {
-        return state.PokemonReducer.pokemonsList;
-    });
-
-    const currentPokemonsListPage = useSelector((state) => {
-        return state.PokemonReducer.currentPokemonsListPage;
-    });
-    const pageOffset = useSelector((state) => {
-        return state.PokemonReducer.pageOffset;
-    });
-    const pageLimit = useSelector((state) => {
-        return state.PokemonReducer.pageLimit;
-    });
-    const allPokemonsCount = useSelector((state) => {
-        return state.PokemonReducer.allPokemonsCount;
-    });
-
-
-    useEffect(() => {
-        const initialise = async () => {
-            await dispatch(
-                fetchPokemonsList({ offset: pageOffset, limit: pageLimit })
-            );
-
-            console.log("abcabc", allPokemonsList);
-        };
-        initialise();
-    }, []);
-
-    useEffect(() => {
-        console.log('abcabc', allPokemonsList)
-        if(!isEmpty(allPokemonsList[currentPokemonsListPage])) {
-            setCurrentlyDisplayedPokemonsList(allPokemonsList[currentPokemonsListPage]);
-        }
-    }, [allPokemonsList]);
-
-    const onChangePage = (_, newPage) => {
-        dispatch(setCurrentPokemonsListPage(newPage));
-        if (isEmpty(allPokemonsList[newPage])) {
-            dispatch(
-                fetchPokemonsList({
-                    offset: (newPage - 1) * pageLimit,
-                    limit: pageLimit,
-                })
-            );
-        } else {
-            setCurrentlyDisplayedPokemonsList(allPokemonsList[newPage]);
-        }
+  const {
+    allPokemonsList,
+    currentPokemonsListPage,
+    pageOffset,
+    pageLimit,
+    allPokemonsCount,
+  } = useSelector((state) => {
+    return {
+      allPokemonsList: state.PokemonReducer.pokemonsList,
+      currentPokemonsListPage: state.PokemonReducer.currentPokemonsListPage,
+      pageOffset: state.PokemonReducer.pageOffset,
+      pageLimit: state.PokemonReducer.pageLimit,
+      allPokemonsCount: state.PokemonReducer.allPokemonsCount,
     };
+  });
 
-    const onPokemonClick = (pokemonName) => {
-        dispatch(fetchPokemonDetail(pokemonName));
-        history.push(`/pokemon/${pokemonName}`);
+  useEffect(() => {
+    const initialise = async () => {
+      await dispatch(
+        fetchPokemonsList({ offset: pageOffset, limit: pageLimit })
+      );
     };
+    initialise();
+  }, []);
 
-    return (
-        <div className="Global-content">
-            {currentlyDisplayedPokemonsList.map((pokemon) => {
-                return (
-                    <Card
-                        label={toTitleCase(pokemon.name)}
-                        labelVariant="h6"
-                        onClick={() => onPokemonClick(pokemon.name)}
-                    />
-                );
-            })}
-            <Pagination
-                onChange={onChangePage}
-                page={currentPokemonsListPage}
-                pages={ceiling(allPokemonsCount / pageLimit)}
-            />
-        </div>
-    );
+  useEffect(() => {
+    console.log("abcabc", allPokemonsList);
+    if (!isEmpty(allPokemonsList[currentPokemonsListPage])) {
+      setCurrentlyDisplayedPokemonsList(
+        allPokemonsList[currentPokemonsListPage]
+      );
+    }
+  }, [allPokemonsList]);
+
+  const onChangePage = (_, newPage) => {
+    dispatch(setCurrentPokemonsListPage(newPage));
+    if (isEmpty(allPokemonsList[newPage])) {
+      dispatch(
+        fetchPokemonsList({
+          offset: (newPage - 1) * pageLimit,
+          limit: pageLimit,
+        })
+      );
+    } else {
+      setCurrentlyDisplayedPokemonsList(allPokemonsList[newPage]);
+    }
+  };
+
+  const onPokemonClick = (pokemonName) => {
+    dispatch(fetchPokemonDetail(pokemonName));
+    history.push(`/pokemon/${pokemonName}`);
+  };
+
+  return (
+    <div className="Global-content">
+      {currentlyDisplayedPokemonsList.map((pokemon) => {
+        return (
+          <Card
+            label={toTitleCase(pokemon.name)}
+            labelVariant="h6"
+            onClick={() => onPokemonClick(pokemon.name)}
+          />
+        );
+      })}
+      <Pagination
+        onChange={onChangePage}
+        page={currentPokemonsListPage}
+        pages={ceiling(allPokemonsCount / pageLimit)}
+      />
+    </div>
+  );
 };
 
 export default PokemonsList;
